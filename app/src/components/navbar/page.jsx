3cf11902f9382f9a9030e8efb97228384cfcb5bd
hassanali01 <hassanali012233@gmail.com>
2024-10-02 11:25:48 +0500

    contact-page

---
+++ app/src/components/navbar/page.jsx
@@ -0,0 +1,217 @@
+"use client";
+
+import { useState,useEffect,useRef } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUser, faUsers,faInfoCircle, faTools, faBlog, faCogs,faUserTie, faHandshake, faQuestionCircle, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import hrsLogo from '@/app/assests/hrs-logo.png'
+const Navbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null); 
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+
+  // Handle click outside the dropdown
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);  // Close the dropdown if clicked outside
+      }
+    };
+
+    // Add event listener to detect clicks outside
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      // Cleanup the event listener when component unmounts
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+  window.addEventListener('scroll', function () {
+    const sections = document.querySelectorAll('section');
+    const scrollPosition = window.scrollY;
+    
+    sections.forEach((section, index) => {
+      const sectionOffsetTop = section.offsetTop; // Section's top position relative to the document
+      
+      // Blur the section only if the user has scrolled past it
+      if (scrollPosition > sectionOffsetTop) {
+        // Calculate blur based on how much the user has scrolled past the section
+        let blurValue = Math.min(10, (scrollPosition - sectionOffsetTop) / 300); // Adjust 50 for sensitivity
+        section.style.filter = `blur(${blurValue}px)`;
+        
+        // Optional: Adjust opacity based on blur
+        let opacityValue = 1 - (blurValue / 10); // Higher blur reduces opacity
+        section.style.opacity = opacityValue;
+      } else {
+        // Reset blur and opacity if the section is not scrolled past
+        section.style.filter = 'none';
+        section.style.opacity = 1;
+      }
+    });
+  });
+  
+  return (
+    <>
+    <header className="rounded-2xl fixed flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full top-10 bg-header-hrs ">
+      <nav
+        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <img src={hrsLogo.src} alt="HRSCamp" className="h-12 my-2 " />
+          </Link>
+        </div>
+    <div className="relative ">
+      <button
+        onClick={toggleDropdown}
+        className="p-1 border border-grey rounded-md bg-slate-200 hover:bg-slate-300 hover:opacity-70 transition-all duration-300"
+      >
+        <FontAwesomeIcon icon={faBars} className="w-7 h-5" />
+       
+    
+      </button>
+
+      {isDropdownOpen && (
+        <div
+        ref={dropdownRef} 
+          id="navbar-dropdown"
+          className={`absolute top-6 right-full mt-2 bg-slate-900 opacity-80 shadow-md rounded-md overflow-hidden ml-4 opacity-0 transition-opacity duration-500 ease-out ${
+            isDropdownOpen ? "opacity-100" : ""
+          }`}
+          style={{ width: "300px" }}
+        >
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              href="/about"
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+            >
+              <span className="flex">
+                {/* <FcAbout className="mt-1 mr-2 mb-1" /> */}
+                <FontAwesomeIcon icon={faInfoCircle} className="mt-1 mr-2 mb-1" />
+                About
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+                 href="/team"
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+            >
+              <span className="flex">
+                {/* <GiTeamIdea className="mt-1 mr-2 mb-1" /> */}
+                <FontAwesomeIcon icon={faUsers} className="mt-1 mr-2 mb-1" />
+                Team
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+                 href="/client"
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+            >
+              <span className="flex">
+              <FontAwesomeIcon icon={faUserTie} className="mt-1 mr-2 mb-1" />
+                {/* <RiTeamFill className="mt-1 mr-2 mb-1" /> */}
+                Client
+              </span>
+            </Link>
+
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/candidate"
+            >
+              <span className="flex">
+                {/* <FaPerson className="mt-1 mr-2 mb-1" /> */}
+                <FontAwesomeIcon icon={faUser} className="mt-1 mr-2 mb-1" />
+                Candidate
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/services"
+            >
+              <span className="flex">
+              <FontAwesomeIcon icon={faTools} className="mt-1 mr-2 mb-1" />
+                
+                {/* <GrServices className="mt-1 mr-2 mb-1" /> */}
+                Services
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/model"
+            >
+              <span className="flex">  
+              <FontAwesomeIcon icon={faCogs} className="mt-1 mr-2 mb-1" />
+
+                {/* <MdModelTraining className="mt-1 mr-2 mb-1" /> */}
+                Model
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/blog"
+            >
+              <span className="flex">
+              <FontAwesomeIcon icon={faBlog} className="mt-1 mr-2 mb-1" />
+                {/* <FaBlog className="mt-1 mr-2 mb-1" /> */}
+                Blog
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/faqs"
+            >
+              <span className="flex">
+              <FontAwesomeIcon icon={faQuestionCircle} className="mt-1 mr-2 mb-1" />
+                {/* <FcFaq className="mt-1 mr-2 mb-1" /> */}
+                FAQs
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          <div className="ml-2 mr-2 mt-3">
+            <Link
+              className="font-medium text-white hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                 href="/contact"
+            >
+              <span className="flex">
+              <FontAwesomeIcon icon={faEnvelope} className="mt-1 mr-2 mb-1" />
+                {/* <MdContacts className="mt-1 mr-2 mb-1" /> */}
+                Contact
+              </span>
+            </Link>
+            <hr className="bg-slate-500" />
+          </div>
+          {/* Add other links as needed */}
+        </div>
+      )}
+    </div>
+    </nav>
+    </header>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ app/src/pages/contact/page.js
@@ -0,0 +1,395 @@
+'use client';
+import React,{useState} from 'react';
+import Image from 'next/image';
+import getInTouchImg from '@/app/assests/contactImg/get-in-touch.png'; // Adjust the path as per your file structure
+import '@/app/globals.css'
+import Head from 'next/head';
+import globleImg from '@/app/assests/contactImg/globle-img.png'
+import headOfficeImg from '@/app/assests/contactImg/head-office.png'
+import ukOffice from '@/app/assests/contactImg/uk-office.png'
+import twitterImg from '@/app/assests/contactImg/twitter.png'
+import linkdinImg from '@/app/assests/contactImg/linkdin.png'
+import letsTalkImg from '@/app/assests/contactImg/lets-talk.png'
+import facebookImg from '@/app/assests/contactImg/facebook.png'
+const Contact = () => {
+  const [step, setStep] = useState(0); // Initialize the step state
+  const [formValues, setFormValues] = useState({
+    floating_first_name: "",
+    floating_email: "",
+    floating_phone: "",
+    floating_subject: "",
+    floating_message: "",
+  });
+
+  const steps = [
+    { label: "What's your name ?", name: "floating_first_name" },
+    { label: "What's your email ?", name: "floating_email" },
+    { label: "What's your phone number ?", name: "floating_phone" },
+    { label: "Subject", name: "floating_subject" },
+    { label: "Message", name: "floating_message" },
+  ];
+
+  const handleChangeStep = (direction) => {
+    if (direction === "next") {
+      setStep(step + 1);
+    } else {
+      setStep(step - 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <main>
+        <section className="section-h section-h-getintouch fix-height" >
+      <div className="container">
+        <div className="get-in-touch">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="get-in-headings">
+                <h1 className="main-heading">
+                  Get in <br />
+                  Touch with <br />
+                  HRS Camp!
+                </h1>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="hrs-camp-h">
+                <Image className="img-heading" src={getInTouchImg} alt="Get in touch" />
+                <div className="get-in-paragraph">
+                  <p>
+                    We're here to assist you in every step of your HR and recruitment journey. We have a global
+                    presence with offices strategically located in key regions, ensuring accessibility to our services.
+                    You can find our office addresses and contact details listed here for your convenience. For
+                    inquiries, feedback, or partnership opportunities, our user-friendly contact form is at your
+                    service. Just drop us a message, and our dedicated team will promptly respond to your needs. Stay
+                    updated with HRS Camp's latest updates and industry insights by following our social media profiles.
+                    Connect with us on platforms like Facebook, Instagram, LinkedIn, and more to engage in meaningful
+                    conversations and stay connected with the pulse of talent acquisition trends. Your feedback and
+                    inquiries are essential to us, and we look forward to hearing from you.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Head>
+        <title>Global Reach - Local Connection</title>
+        <meta name="description" content="HRS Camp's global reach with local offices." />
+      </Head>
+      <section className="section-h section-h-getintouch" style={{margin:"80px 0px"}}>
+        <div className="container">
+          <div className="get-in-touch">
+            <div className="row g-3 align-items-center">
+              <div className="col-lg-6">
+                <div className="get-in-headings">
+                  <h1 className="main-heading">
+                    Our Global <br />
+                    Reach, Your <br />
+                    Local Connection
+                  </h1>
+                </div>
+              </div>
+              <div className="col-lg-6 section-adjust">
+                <div className='section-one'>
+                <Image
+                  className=" img-heading"
+                  src={globleImg}
+                  alt="Global Reach"
+                  width={600}
+                  height={400}
+                />
+                <div className="get-in-paragraph">
+                  <p>
+                    Explore HRS Camp's network of strategically placed offices, offering global reach
+                    with a local touch. Our offices ensure accessibility to top-tier talent and
+                    personalized recruitment solutions tailored to your region. Contact us to harness
+                    the power of our global expertise.
+                  </p>
+                </div>
+                </div>
+                <div className='section-two'>
+                <h1 className="location-heading my-3">Our Offices</h1>
+                <div className="row g-3">
+                  <div className="col-lg-6">
+                    <div className="location-cards shadow-sm">
+                      <div>
+                        <Image
+                          className="w-100"
+                          src={headOfficeImg}
+                          alt="Head Office"
+                          width={400}
+                          height={300}
+                        />
+                        <div className="location-text">
+                          <h4 className="sub-heading">Head Office</h4>
+                          <div>
+                            <ul className="d-flex align-items-center">
+                              <li>
+                                <a href="#">
+                                  <i className="fas fa-globe"></i>
+                                </a>
+                              </li>
+                              <li className="ms-2">
+                                Faiz Ahmed Faiz Rd, Quaid-e-Azam Industrial Estate, Lahore, Punjab
+                                54000, Pakistan
+                              </li>
+                            </ul>
+                            <ul className="d-flex align-items-center">
+                              <li>
+                                <a href="tel:+924231112223">
+                                  <i className="fas fa-phone"></i>
+                                </a>
+                              </li>
+                              <li className="ms-2"> +92 423 111 22 23</li>
+                            </ul>
+                            <ul className="d-flex align-items-center">
+                              <li>
+                                <a href="mailto:contact@hracamp.com">
+                                  <i className="fas fa-envelope"></i>
+                                </a>
+                              </li>
+                              <li className="ms-2"> contact@hracamp.com</li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-6">
+                    <div className="location-cards d-flex flex-column shadow-sm">
+                      <div>
+                        <Image
+                          className="w-100"
+                          src={ukOffice}
+                          alt="UK Office"
+                          width={400}
+                          height={300}
+                        />
+                        <div className="location-text">
+                          <h4 className="sub-heading">UK Office</h4>
+                          <div>
+                            <ul className="d-flex align-items-center">
+                              <li>
+                                <a href="#">
+                                  <i className="fas fa-globe"></i>
+                                </a>
+                              </li>
+                              <li className="ms-2"> 1 Hyde Park, London, United Kingdom</li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Head>
+        <title>Seamless Communication, Swift Solutions</title>
+        <meta
+          name="description"
+          content="Contact us for inquiries, feedback, or partnership requests. Our team will provide prompt, tailored solutions."
+        />
+      </Head>
+      <section className="section-h section-h-getintouch">
+        <div className="container">
+          <div className="get-in-touch">
+            <div className="row g-3 align-items-center">
+              <div className="col-lg-6">
+                <div className="get-in-headings">
+                  <h1 className="main-heading">
+                    Seamless <br />
+                    Communication,<br />
+                    Swift Solutions
+                  </h1>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <Image
+                  className="img-heading"
+                  src={getInTouchImg}
+                  alt="Get In Touch"
+                  width={600}
+                  height={400}
+                />
+                <div className="get-in-paragraph">
+                  <p>
+                    Our user-friendly contact form ensures hassle-free communication. Send us your
+                    inquiries, feedback, or partnership requests, and our dedicated team will provide
+                    prompt, tailored solutions to meet your needs. Reach out today for a seamless
+                    exchange of information.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <div className="mx-3 h-[50rem] ">
+        <form className="pt-[25rem]">
+          <div className="relative flex z-0 w-full mb-6 group">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="text"
+                name={steps[step].name}
+                id={steps[step].name}
+                value={formValues[steps[step].name]}
+                className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                onChange={handleInputChange}
+                required
+              />
+              <label
+                htmlFor={steps[step].name}
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                {steps[step].label}
+              </label>
+
+              <progress
+                value={(step + 1) * (100 / steps.length)} // Calculate the value dynamically
+                max="100"
+                className="block w-full bg-blue-500 h-[0.1rem]"
+              />
+            </div>
+            <button
+              type="button"
+              className="text-black font-bold bg  hover:text-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center  dark:focus:ring-blue-800"
+              onClick={() => handleChangeStep("next")}
+              disabled={step === steps.length - 1}
+            >
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+
+
+      <section className="section-h section-h-getintouch">
+    <div className="container">
+      <div className="contact-us">
+        <div className="row g-3">
+          <div className="col-lg-6"></div>
+          <div className="col-lg-6 d-flex justify-content-end">
+            <div className="contact-content d-flex flex-column">
+              <h2>Contact US</h2>
+              <p className="get-in-paragraph">hello@hrscamp.com</p>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <Head>
+        <title>Stay Connected, Stay Informed</title>
+        <meta
+          name="description"
+          content="Connect with HRS Camp on social media to stay updated on industry insights, job market trends, and career opportunities."
+        />
+      </Head>
+      <section className="section-h section-h-getintouch">
+        <div className="container">
+          <div className="get-in-touch">
+            <div className="row g-3 align-items-center">
+              <div className="col-lg-6">
+                <div className="get-in-headings">
+                  <h1 className="main-heading">
+                    Stay Connected, <br />
+                    Stay Informed
+                  </h1>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <Image
+                  className="img-heading"
+                  src={getInTouchImg}
+                  alt="Stay Connected"
+                  width={600}
+                  height={400}
+                />
+                <div className="get-in-paragraph my-3">
+                  <p>
+                    Connect with HRS Camp on our dynamic social media pages. Follow us on Facebook, Instagram,
+                    LinkedIn, and more to access the latest industry insights, job market trends, and exciting
+                    career opportunities. Join our online community and stay informed with the click of a button.
+                  </p>
+                </div>
+                <div className="social-icons">
+                  <ul className="d-flex">
+                    <li>
+                      <a href="#">
+                        <Image
+                          className="img-fluid"
+                          src={facebookImg}
+                          alt="Facebook"
+                          width={90}
+                          height={90}
+                        />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <Image
+                          className="img-fluid"
+                          src={twitterImg}
+                          alt="Twitter"
+                          width={90}
+                          height={90}
+                        />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <Image
+                          className="img-fluid"
+                          src={linkdinImg}
+                          alt="LinkedIn"
+                          width={90}
+                          height={90}
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className=" section-h section-h-getintouch">
+  <div className="d-flex align-items-center">
+    <Image className="img-fluid" src={letsTalkImg} alt=""/>
+  </div>
+</section>
+    </main>
+
+  
+  );
+};
+
+export default Contact ;
